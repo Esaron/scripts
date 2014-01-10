8b1de3d0f90d51e5c81cc67cbe6199ca7ca0b8a1
Esaron <spam.esaron@gmail.com>
2014-01-10 10:24:04 -0500

    Added retries now that low res images are rejected
    Change-Id: I347692ad87893b76ab8444eb2e91a2066a310e0a

--- redditWallpaper.sh
+++ redditWallpaper.sh
@@ -12,40 +12,45 @@ IMGPATTERN='http://\(i.\)\?imgur.com/\([0-9a-zA-Z_-]*\)\(.jpg\)\?'
 RAWURLS=$(curl --silent $FEED | grep -o -e $IMGPATTERN)
 echo $RAWURLS
 URLS=($RAWURLS)
-URL=${URLS[$(( $RANDOM % ${#URLS[@]} ))]}
+COUNTER=5
+while [ $COUNTER -gt 0 ] ; do
+  URL=${URLS[$(( $RANDOM % ${#URLS[@]} ))]}
 
-if [ "$(echo $URL | grep 'http://i.' )" == "" ] ; then
-  URL="http://i.${URL:7:${#URL}}"
-fi
-if [ "$(echo $URL | grep '.jpg' )" == "" ] ; then
-  URL="$URL.jpg"
-fi
-IMG=$(basename "$URL")
-echo $URL
-IMGPATH=$(find $BASEPATH -type f -name *$IMG)
-if [ -z "$IMGPATH" ] ; then
-  IMGURL=$URL
-  #IMGPATH=${BASEPATH}${DATEPREFIX}-${TIMEPREFIX}_${IMG}
-  IMGPATH=${BASEPATH}${IMG}
-else
-  echo "$IMGURL already downloaded."
-fi
-if [ ! -z "$IMGPATH" ] ; then
-  if [ ! -z "$IMGURL" ] ; then
-    wget -O "$IMGPATH" "$IMGURL"
-    attr -s url -V "$IMGURL" "$IMGPATH"
+  if [ "$(echo $URL | grep 'http://i.' )" == "" ] ; then
+    URL="http://i.${URL:7:${#URL}}"
   fi
-  IMGWIDTHHEIGHT=$(identify -format "%w,%h" $IMGPATH)
-  IMGWIDTH=$(echo $IMGWIDTHHEIGHT | cut -d "," -f 1)
-  IMGHEIGHT=$(echo $IMGWIDTHHEIGHT | cut -d "," -f 2)
-  if [[ $IMGWIDTH -gt 1100 && $IMGHEIGHT -gt 700 ]] ; then
-    env DISPLAY=:0 gsettings set org.gnome.desktop.background picture-uri "file://$IMGPATH"
+  if [ "$(echo $URL | grep '.jpg' )" == "" ] ; then
+    URL="$URL.jpg"
+  fi
+  IMG=$(basename "$URL")
+  echo $URL
+  IMGPATH=$(find $BASEPATH -type f -name *$IMG)
+  if [ -z "$IMGPATH" ] ; then
+    IMGURL=$URL
+    #IMGPATH=${BASEPATH}${DATEPREFIX}-${TIMEPREFIX}_${IMG}
+    IMGPATH=${BASEPATH}${IMG}
   else
-    echo "Not setting desktop to $IMGPATH as image is too small (${IMGWIDTH}x${IMGHEIGHT})"
+    echo "$IMGURL already downloaded."
   fi
-  #OLDFILES=$(find "$BASEPATH" -type f -not -name "$DATEPREFIX*" -delete)
-  #for FILE in $OLDFILES
-  #do
-    #rm $FILE
-  #done
-fi
+  if [ ! -z "$IMGPATH" ] ; then
+    if [ ! -z "$IMGURL" ] ; then
+      wget -O "$IMGPATH" "$IMGURL"
+      attr -s url -V "$IMGURL" "$IMGPATH"
+    fi
+    IMGWIDTHHEIGHT=$(identify -format "%w,%h" $IMGPATH)
+    IMGWIDTH=$(echo $IMGWIDTHHEIGHT | cut -d "," -f 1)
+    IMGHEIGHT=$(echo $IMGWIDTHHEIGHT | cut -d "," -f 2)
+    if [[ $IMGWIDTH -gt 1100 && $IMGHEIGHT -gt 700 ]] ; then
+      env DISPLAY=:0 gsettings set org.gnome.desktop.background picture-uri "file://$IMGPATH"
+      COUNTER=0
+    else
+      echo "Not setting desktop to $IMGPATH as image is too small (${IMGWIDTH}x${IMGHEIGHT})"
+      let COUNTER=$COUNTER-1
+    fi
+    #OLDFILES=$(find "$BASEPATH" -type f -not -name "$DATEPREFIX*" -delete)
+    #for FILE in $OLDFILES
+    #do
+      #rm $FILE
+    #done
+  fi
+done
