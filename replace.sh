3722fa7abb14254b9fb5779da53e05324e17b2c6
Jon Marshall <jon.marshall@betterup.co>
2025-03-07 10:21:00 -0500

    Make replace cross platform and a little simpler

--- replace.sh
+++ replace.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
-# Replace:
-#  Arg 1: To replace
-#  Arg 2: Replace with
-#  Arg 3+: Target dirs/files (default to current)
+usage() {
+    echo "Usage: $0 <directory> <search_string> <replace_string> [-d]"
+    echo "  -d : Delete backup files (only applies on macOS/BSD sed)"
+    exit 1
+}
 
-TOREPLACE=$1
-REPLACEMENT=$2
-if [[ $TOREPLACE == */* || $REPLACEMENT == */* ]]
-then
-  echo "Error: Either TOREPLACE or REPLACE contains \"/\" used for sed pattern delimiter."
-  exit 1
-fi
-shift 2
-
-if [[ $# == 0 ]]
-then
-  TARGETS="."
-else
-  TARGETS="$@"
+if [[ $# -lt 3 ]]; then
+    usage
 fi
 
-echo "Replacing \"$TOREPLACE\" with \"$REPLACEMENT\" in all of the following:"
-echo "$TARGETS"
+DIR=$1
+SEARCH=$2
+REPLACE=$3
+shift 3
 
-find $TARGETS -type f -exec sed -i '' -e "s/$TOREPLACE/$REPLACEMENT/g" {} +
+DELETE_BACKUP=false
+
+while getopts "d" opt; do
+    case ${opt} in
+        d) DELETE_BACKUP=true ;;
+        *) usage ;;
+    esac
+done
+
+# Find and replace in all files
+find "$DIR" -type f | while IFS= read -r file; do
+    sed -i.bak "s/${SEARCH}/${REPLACE}/g" "$file"
+done
+
+if [[ "$DELETE_BACKUP" == true ]]; then
+    find "$DIR" -type f -name "*.bak" -delete
+fi
+
+echo "Replacement complete."
 
